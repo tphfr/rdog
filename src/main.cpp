6970b4c55359ca879c4c18b00c919ea5002e5767
nathan87104 <t.phithiwat@gmail.com>
2025-08-30 00:26:47 +0700

    stored old code new

--- src/main.cpp
+++ src/main.cpp
@@ -4,60 +4,61 @@
 // The default power supply voltage set by the program is 12V, please remember to modify the values in voltage_power_supply and voltage_limit variables if you use other voltages for power supply
 
 #include <SimpleFOC.h>
+#include <cmath>
+#include <tuple>
 
-BLDCMotor motor = BLDCMotor(11);
-BLDCDriver3PWM driver = BLDCDriver3PWM(32,33,25);
-  
-// BLDC motor & driver instance
-BLDCMotor motor1 = BLDCMotor(11);
-BLDCDriver3PWM driver1  = BLDCDriver3PWM(26,27,14);
+double position[3] = {0, 0, 0};
 
-//Target variable
-float target_velocity = 3.14;
+//distance from hip's rotation axis to the knee driver gear, all in meters
+double hipLength = 0.15;
+double thighLength = 0.25;
+double kneeLength = 0.25;
 
-//Serial command setting
-Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_velocity, cmd); }
+double degAngles[3] = {0, 0, 0};
 
-void setup() {
-  Serial.begin(9600);
+BLDCMotor FRHmotor = BLDCMotor(11);
+BLDCDriver3PWM FRHdriver = BLDCDriver3PWM(32,33,25);
 
-  driver.voltage_power_supply = 9;
-  driver.voltage_limit = 9;
-  driver.init();
-  motor.linkDriver(&driver);
-  motor.voltage_limit = 2;   // [V]
-  motor.velocity_limit = 100; // [rad/s]
-  
-  driver1.voltage_power_supply = 9;
-  driver1.voltage_limit = 9;
-  driver1.init();
-  motor1.linkDriver(&driver1);
-  motor1.voltage_limit = 2;   // [V]
-  motor1.velocity_limit = 40; // [rad/s]
+BLDCMotor FRTmotor = BLDCMotor(11);
+BLDCDriver3PWM FRTdriver  = BLDCDriver3PWM(26,27,14);
 
- 
-  //Open loop control mode setting
-  motor.controller = MotionControlType::velocity_openloop;
-  motor1.controller = MotionControlType::angle_openloop;
+BLDCMotor FRKmotor = BLDCMotor(11);
+BLDCDriver3PWM FRKdriver  = BLDCDriver3PWM(26,27,14);
 
-  //Initialize the hardware
-  motor.init();
-  //motor1.init();
+// Constants: t, h, k
+// Input: x, y, z
+// Output: theta1, theta2, theta3 (in degrees, shifted origin)
 
-  //Add T command
-  command.add('T', doTarget, "target velocity");
+std::tuple<double,double,double> inverseKinematics(double x, double y, double z) {
+    // Step 1: compute r and theta1
+    double r = sqrt(x*x + y*y);
+    double theta1 = atan2(x, -y) * 180.0 / M_PI; // degrees
 
-  Serial.begin(9600);
-  Serial.println("Motor ready!");
-  Serial.println("Set target velocity [rad/s]");
-  _delay(1000);
+    // Step 2: solve for theta3 from law of cosines
+    double d = sqrt(x*x + y*y) - hipLength;
+    double D = (thighLength*thighLength + kneeLength*kneeLength - d*d) / (2*thighLength*kneeLength);
+    if(D > 1) D = 1; 
+    if(D < -1) D = -1;
+    double theta3 = acos(D) * 180.0 / M_PI; // degrees
+
+    // Step 3: solve for theta2 from z-equation
+    double A = thighLength - kneeLength * cos(theta3 * M_PI/180.0);
+    double B = kneeLength * sin(theta3 * M_PI/180.0);
+    double theta2 = atan2(z - A*cos(0), B) * 180.0 / M_PI; // simplified form
+
+    // Step 4: apply origin shift (θ1=90, θ2=40, θ3=85 as zero reference)
+    theta1 -= 90.0;
+    theta2 -= 40.0;
+    theta3 -= 85.0;
+
+    return {theta1, theta2, theta3};
 }
 
-void loop() {
-  motor.move(target_velocity);
-  //motor1.move(target_velocity);
 
-  //User newsletter
-  command.run();
+void setup () {
+  Serial.begin(9600);
+  Serial.println("Inverse Kinematics Ready");
+  Serial.println(std::get<0>(inverseKinematics(0,0,0)));
+  Serial.println(std::get<1>(inverseKinematics(0,0,0)));
+  Serial.println(std::get<2>(inverseKinematics(0,0,0)));
 }

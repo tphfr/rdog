08d89a5a977245b736d635d3f6f811438822d455
tphfr <t.phithiwat@gmail.com>
2026-01-17 22:16:11 +0700

    THIS DOES WORK

--- src/getAddress.cpp
+++ src/getAddress.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <array>
 
+int id;
 
 MagneticSensorI2C RIGHTsensor = MagneticSensorI2C(AS5600_I2C);
 MagneticSensorI2C LEFTsensor = MagneticSensorI2C(AS5600_I2C);
@@ -14,10 +15,10 @@ TwoWire I2Ctwo = TwoWire(1);
 
 //Motor parameters
 BLDCMotor RIGHTmotor = BLDCMotor(11);
-BLDCDriver3PWM RIGHTdriver = BLDCDriver3PWM(32,33,25);
+BLDCDriver3PWM RIGHTdriver = BLDCDriver3PWM(26,27,14);
 
 BLDCMotor LEFTmotor = BLDCMotor(11);
-BLDCDriver3PWM LEFTdriver = BLDCDriver3PWM(26,27,14);
+BLDCDriver3PWM LEFTdriver = BLDCDriver3PWM(32,33,25);
 
 //Command settings
 
@@ -66,14 +67,18 @@ void addMaster() {
   
 }
 
+
+
 void initialize() {
+
   I2Cone.begin(19,18, 400000); 
   I2Ctwo.begin(23,5, 400000);
-  RIGHTsensor.init(&I2Cone);
-  LEFTsensor.init(&I2Ctwo);
+  LEFTsensor.init(&I2Cone);
+  RIGHTsensor.init(&I2Ctwo);
   //Connect the motor object with the sensor object
-  RIGHTmotor.linkSensor(&RIGHTsensor);
   LEFTmotor.linkSensor(&LEFTsensor);
+  RIGHTmotor.linkSensor(&RIGHTsensor);
+
   //Supply voltage setting [V]
   RIGHTdriver.pwm_frequency = 50000;
   RIGHTdriver.voltage_power_supply = 9;
@@ -82,92 +87,92 @@ void initialize() {
   LEFTdriver.pwm_frequency = 50000;
   LEFTdriver.voltage_power_supply = 9;
   LEFTdriver.init();
+
   //Connect the motor and driver objects
-  RIGHTmotor.linkDriver(&RIGHTdriver);
   LEFTmotor.linkDriver(&LEFTdriver);
-  
+  RIGHTmotor.linkDriver(&RIGHTdriver);
+
   //FOC model selection
-  RIGHTmotor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   LEFTmotor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+  RIGHTmotor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   //Motion Control Mode Settings
-  RIGHTmotor.controller = MotionControlType::angle;
   LEFTmotor.controller = MotionControlType::angle;
+  RIGHTmotor.controller = MotionControlType::angle;
 
   //Speed PI loop setting
+  LEFTmotor.PID_velocity.P = 0.1;
   RIGHTmotor.PID_velocity.P = 0.1;
-  LEFTmotor.PID_velocity.P = 0.05;
-  RIGHTmotor.PID_velocity.I = 0.4;
-  LEFTmotor.PID_velocity.I = 0.4;
+  LEFTmotor.PID_velocity.I = 1;
+  RIGHTmotor.PID_velocity.I = 1;
 
   //Angle P ring setting
-  RIGHTmotor.P_angle.P = 1;
   LEFTmotor.P_angle.P = 1;
-  //Speed low-pass filter time constant
-  RIGHTmotor.LPF_velocity.Tf = 0.01;
-  LEFTmotor.LPF_velocity.Tf = 0.01;
+  RIGHTmotor.P_angle.P = 1;
 
-  
+  //Speed low-pass filter time constant
+  LEFTmotor.LPF_velocity.Tf = 0.01;
+  RIGHTmotor.LPF_velocity.Tf = 0.01;
   
   //Max motor limit motor
-  RIGHTmotor.voltage_limit = 3;
-  RIGHTmotor.current_limit = 6;
   LEFTmotor.voltage_limit = 3;
   LEFTmotor.current_limit = 6;
+  RIGHTmotor.voltage_limit = 3;
+  RIGHTmotor.current_limit = 6;
 
   //Set a maximum speed limit
+  LEFTmotor.velocity_limit = 80;
   RIGHTmotor.velocity_limit = 80;
-  LEFTmotor.velocity_limit = 110;
 
-  Serial.begin(9600);
-  RIGHTmotor.useMonitoring(Serial);
   LEFTmotor.useMonitoring(Serial);
+  RIGHTmotor.useMonitoring(Serial);
 
-  RIGHTmotor.sensor_direction = CW;
-  LEFTmotor.sensor_direction = CCW;
-
-  //Initialize the motor
-  RIGHTmotor.init();
-  //LEFTmotor.init();
-  //Initialize FOC
-  RIGHTmotor.initFOC();
-  //LEFTmotor.initFOC();
   
+  //Initialize the motor
+  LEFTmotor.init();
+  RIGHTmotor.init();
+  //Initialize FOC
+  LEFTmotor.initFOC();
+  RIGHTmotor.initFOC();
+
   Serial.println(F("Motor ready."));
   Serial.println(F("Set the target velocity using serial terminal:"));
   
 }
 
-void updateZero() {
-  zeroOffsetR = RIGHTsensor.getAngle();
-  zeroOffsetL = LEFTsensor.getAngle();
-}
-
-float target = 0.0;
-
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&incomingInformation, incomingData, sizeof(incomingInformation));
   
   if (incomingInformation.command == CMD_INIT) {
-    Serial.println("Initializing Motors and Sensors");
+    if (incomingInformation.thetal == 10000) {
+      id = 0;
+    } else if (incomingInformation.thetal == 10001) {
+      id = 1;
+    } else if (incomingInformation.thetal == 10002) {
+      id = 2;
+    } else if (incomingInformation.thetal == 10003) {
+      id = 3;
+    } else if (incomingInformation.thetal == 10004) {
+      id = 4;
+    } else if (incomingInformation.thetal == 10005) {
+      id = 5;
+    }
+        
     initialize();
     return;
   }
 
   if (incomingInformation.command == CMD_CALIBRATE) {
-    updateZero();
-    state = STATE_WALKING;
+    //updateZero();
     return;
   }
 
   if (incomingInformation.command == CMD_START) {
     state = STATE_WALKING;
-    target = 6.28*10.0;
     return;
   }
 
   if (incomingInformation.command == CMD_STOP) {
-    target = 0.0;
-    state = STATE_WALKING;
+    state = STATE_IDLE;
     return;
   }
 
@@ -178,6 +183,7 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
 
 void setup() {
   Serial.begin(9600);
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -190,20 +196,10 @@ void setup() {
   // Register receive callback
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
   addMaster();
+  
+  initialize();
 }
 
 void loop() {
 
-
-  if (state != STATE_WALKING) {
-    return;
-  }  
-//if true then move motors
-  //Serial.println("Left angle: " + String(thetaL) + " Right angle: " + String(thetaR));
-  RIGHTmotor.loopFOC();
-  //LEFTmotor.loopFOC();
-
-  RIGHTmotor.move(target - zeroOffsetR);
-  //LEFTmotor.move(thetaL);
-  
-  }
+}
